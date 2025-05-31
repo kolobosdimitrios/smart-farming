d8dd504244ddde48a2aa615c9ed82a6c8a3a4736
dimitrisKolovos <kolobosd98@gmail.com>
2025-05-31 16:36:33 +0300

    initial commit,

---
+++ src/main/java/com/dkolovos/smart/farming/core/domain/SmartFarmingCoreDomain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dkolovos.smart.farming.core.domain;
+
+/**
+ *
+ * @author dimitrioskolovos
+ */
+public class SmartFarmingCoreDomain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
